3f9b4860e6eed4216cc6aa3bd424540ce32087bd
masica <masica1920@163.com>
2017-11-05 17:21:07 +0800

    --

--- lib/datasets/factory.py
+++ lib/datasets/factory.py
@@ -12,18 +12,21 @@ from __future__ import print_function
 
 __sets = {}
 from lib.datasets.pascal_voc import pascal_voc
+#from datasets.coco import coco
+from lib.datasets.gridsum_car import gridsum_car
 
+import numpy as np
 
 # Set up voc_<year>_<split> 
-for year in ['2007', '2012', '2013', '2014']:
+for year in ['2007', '2012']:
   for split in ['train', 'val', 'trainval', 'test']:
     name = 'voc_{}_{}'.format(year, split)
     __sets[name] = (lambda split=split, year=year: pascal_voc(split, year))
 
-for year in ['2007', '2012', '2013']:
-  for split in ['train', 'val', 'trainval', 'test']:
-    name = 'voc_{}_{}_diff'.format(year, split)
-    __sets[name] = (lambda split=split, year=year: pascal_voc(split, year, use_diff=True))
+# Set up gridsum_car_<split>
+for split in ['train', 'val', 'trainval', 'test']:
+  name = '{}_{}'.format('gridsum_car', split)
+  __sets[name] = (lambda split=split : gridsum_car(split))
 
 
 def get_imdb(name):
